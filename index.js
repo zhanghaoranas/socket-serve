7244d77d6d480acc86fc574937ed1dc57bc383b8
haoran <1243226736@qq.com>
2021-08-17 18:59:09 +0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socket = require("socket.io");
+const port = process.env.PORT || 3000; // default port: 3000
+
+const app = express();
+const server = http.createServer(app); // use express to handle http server
+const io = socket(server);
+
+const onConnection = (socket) => {
+  console.log("Socket.io init success");
+};
+
+io.on("connection", onConnection);
+
+server.listen(port, () => {
+  console.log("Server listening at port %d", port);
+});
